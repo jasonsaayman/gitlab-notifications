56d84cbf78304e2441267af382f280c484771090
Jay <jasonsaayman@gmail.com>
2023-06-22 20:26:42 +0200

    wip: convert to work for gitlab

--- src/components/fields/FieldInput.tsx
+++ src/components/fields/FieldInput.tsx
@@ -31,7 +31,7 @@ export const FieldInput: React.FC<IProps> = ({
 
           <input
             type={type}
-            className="appearance-none block w-full rounded-sm bg-gray-700 text-white text-sm py-1.5 px-4 mb-2 focus:outline-none"
+            className="appearance-none block w-full rounded-sm bg-gray-700 text-white text-xs py-1.5 px-4 mb-2 focus:outline-none"
             id={input.name}
             placeholder={placeholder}
             required={required}
@@ -39,7 +39,7 @@ export const FieldInput: React.FC<IProps> = ({
           />
 
           {helpText && (
-            <div className="mt-3 text-gray-700 dark:text-gray-200 text-xs">
+            <div className="mt-3 text-white text-xs">
               {helpText}
             </div>
           )}
