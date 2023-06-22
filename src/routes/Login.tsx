adc05e971431f9f107899ffc00c4957fba41ccae
Jay <jasonsaayman@gmail.com>
2023-06-22 20:08:48 +0200

    wip: convert to work for gitlab

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,13 +1,23 @@
 const { ipcRenderer } = require('electron');
 
-import React, { useCallback, useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { Form, FormRenderProps } from 'react-final-form';
+import { shell } from 'electron';
 
 import { AppContext } from '../context/App';
+import { AuthTokenOptions } from '../types';
+import { FieldInput } from '../components/fields/FieldInput';
+interface IValues {
+  token?: string;
+  hostname?: string;
+}
+
 
 export const LoginRoute: React.FC = () => {
   const history = useHistory();
-  const { isLoggedIn, login } = useContext(AppContext);
+  const { isLoggedIn, login, validateToken } = useContext(AppContext);
+  const [isValidToken, setIsValidToken] = useState<boolean>(true);
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -24,6 +34,65 @@ export const LoginRoute: React.FC = () => {
     }
   }, []);
 
+  const openLink = useCallback((url: string) => {
+    shell.openExternal(url);
+  }, []);
+
+  const submit = useCallback(async (data: IValues) => {
+    setIsValidToken(true);
+    try {
+      await validateToken(data as AuthTokenOptions);
+      history.goBack();
+    } catch (err) {
+      setIsValidToken(false);
+    }
+  }, []);
+
+  const renderForm = (formProps: FormRenderProps) => {
+    const { handleSubmit, submitting, pristine, values } = formProps;
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <FieldInput
+          name="token"
+          label="Token"
+          placeholder="The 40 characters token generated on GitHub"
+          helpText={
+            <>
+              To generate a token, go to GitHub,{' '}
+              <a
+                className="underline hover:text-gray-500 dark:hover:text-gray-300  cursor-pointer"
+                onClick={() => openLink('https://github.com/settings/tokens')}
+              >
+                personal access tokens
+              </a>{' '}
+              and create one with the{' '}
+              <span className="underline font-extrabold text-yellow-500">
+              </span>
+              scopes.
+            </>
+          }
+        />
+
+        <FieldInput
+          name="hostname"
+          label="Hostname"
+          placeholder="github.company.com"
+          helpText="Defaults to github.com. Change only if you are using GitHub for Enterprise."
+        />
+
+        <button
+          className="float-right px-4 py-2 my-4 bg-gray-300 font-semibold rounded text-sm text-center hover:bg-gray-500 hover:text-white dark:text-black focus:outline-none"
+          disabled={submitting || pristine}
+          type="submit"
+          title="Submit Button"
+        >
+          Submit
+        </button>
+      </form>
+    );
+  };
+
   return (
     <div className="flex flex-1 flex-col justify-center items-center p-4 bg-white dark:bg-gray-dark dark:text-white">
 
@@ -32,13 +101,12 @@ export const LoginRoute: React.FC = () => {
         <span className='font-normal text-sm'>Fill in the values below to get started!</span>
       </div>
 
-      <button
-        className='w-48 py-2 my-2 bg-gray-300 font-semibold rounded text-xs text-center dark:text-black hover:bg-gray-500 hover:text-white focus:outline-none'
-        onClick={loginUser}
-        aria-label="Login with GitHub"
+      <Form
+        onSubmit={submit}
+        // validate={validate}
       >
-        Continue
-      </button>
+        {renderForm}
+      </Form>
     </div>
   );
 };
